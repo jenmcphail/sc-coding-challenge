dd525965f3e883cb2e3f7fe8a8fc8efee309bc4d
jenmcphail <mcphail.jen@gmail.com>
2016-11-29 13:09:20 -0800

    requires angular dependency, creates index.html page, and configures express to serve static assets

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const port       = process.env.PORT || 8080
 // configures body parser
 app.use(bodyParser.urlencoded());
 
+//serves static assets
+app.use(express.static('public'));
+
 //configures server
 app.listen(port);
-console.log('Server running on port' + port)
+console.log('Server running on port ' + port)
