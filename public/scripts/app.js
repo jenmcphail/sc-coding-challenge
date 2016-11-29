afff1efd8dc22e287f9d1d5da4601634888d74e3
jenmcphail <mcphail.jen@gmail.com>
2016-11-29 13:39:34 -0800

    connects app to angular

---
+++ public/scripts/app.js
@@ -0,0 +1,9 @@
+const storeApp = angular.module('storeApp', []);
+
+storeApp.controller('storeCtrl', function($scope){
+  console.log("controller connected")
+});
+
+function storeCtrl ($scope, $http){
+
+};
