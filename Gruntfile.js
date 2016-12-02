b0aa3ecc3a3cf43e959906b0592b672d9989d2f6
jenmcphail <mcphail.jen@gmail.com>
2016-12-02 09:21:16 -0800

    requires grunt and sets up grunt tasks for stylesheet minification and autoprefixing (for flexbox)

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+  pkg: grunt.file.readJSON('package.json'),
+
+  cssmin: {
+  target: {
+    files: [{
+      expand: true,
+      cwd: 'public/styles',
+      src: ['*.css', '!*.min.css'],
+      dest: 'public/styles',
+      ext: '.min.css'
+    }]
+  }
+},
+
+  autoprefixer: {
+  options: {
+    browsers: ['last 2 versions']
+  },
+  multiple_files: {
+    expand: true,
+    flatten: true,
+    src: 'public/styles/*.css',
+    dest: 'public/styles'
+  }
+},
+
+watch: {
+css: {
+  files: '**/*.css',
+  tasks: ['cssmin', 'autoprefixer']
+    }
+  },
+  
+  });
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-autoprefixer');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
+grunt.registerTask('default',['watch']);
+}
