41bb0dd5497dd7f2e8efbbfd011fa97acc4142f4
jenmcphail <mcphail.jen@gmail.com>
2016-12-04 19:41:13 -0800

    adds https to API endpoint request

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,7 +2,7 @@ const storeApp = angular.module('storeApp', []);
 
 storeApp.controller('storeCtrl', function($scope, $http){
   // console.log("controller connected")
-  $http.get('http://sneakpeeq-sites.s3.amazonaws.com/interviews/ce/feeds/store.js')
+  $http.get('https://sneakpeeq-sites.s3.amazonaws.com/interviews/ce/feeds/store.js')
       .success(function(response){
         console.log(response);
         $scope.response = response
